f5b343daf86c1ba5b0a22dc185da3c29bc03f8ac
DigitalPhoenixX <mohamed.xyz@hotmail.com>
2021-08-28 16:32:18 +0200

    feat: morgan

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require("express");
+const morgan = require("morgan");
 
 require("dotenv").config();
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT;
 
 const app = express();
+
+app.use(morgan("dev"));
 
 app.get("/", (req, res) => {
   res.json({ msg: "hello" });
